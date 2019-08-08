03fd0b7d86f049179e831fb17528a5b592a84f09
Varun Horril <varunhorril@outlook.com>
2019-08-08 16:26:33 +0400

    UPDATE PotionType: Added description field for schema.

--- HogwartsPotions/HogwartsPotions/GraphQL/Types/PotionType.cs
+++ HogwartsPotions/HogwartsPotions/GraphQL/Types/PotionType.cs
@@ -12,8 +12,16 @@ namespace HogwartsPotions.GraphQL.Types
         public PotionType()
         {
             Field(t => t.Id);
-            Field(t => t.Name);
-            Field(t => t.Description);
+            Field(t => t.Name).Description("The name of the potion");
+            Field(t => t.Description).Description("The description of the potion");
+            Field(t => t.Price).Description("Price of potion");
+            Field(t => t.Stock).Description("Available number of potions in stock");
+            Field(t => t.IntroducedAt).Description("Who invented the potion? ;)");
+            Field(t => t.Rating).Description("Wizards rating of the potion");
+            Field(t => t.Type).Description("The type of potion");
+
+            Field(t => t.CreatedOn);
+
         }
     }
 }
