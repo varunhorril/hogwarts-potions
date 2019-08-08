4a7b5256519ec1ee653c91907b7a47511e7b3aa2
Varun Horril <varunhorril@outlook.com>
2019-08-08 13:33:30 +0400

    ADD Repository class

---
+++ HogwartsPotions/HogwartsPotions/Repositories/PotionRepository.cs
@@ -0,0 +1,24 @@
+﻿using HogwartsPotions.Data;
+using HogwartsPotions.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Repositories
+{
+    public class PotionRepository
+    {
+        private readonly PotionDbContext _dbContext;
+
+        public PotionRepository(PotionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Potion> GetAll()
+        {
+            return _dbContext.Potions;
+        }
+    }
+}
