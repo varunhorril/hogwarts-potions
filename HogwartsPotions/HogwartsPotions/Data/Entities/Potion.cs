23894505af94ae0263b648208c9766389aabf3ec
Varun Horril <varunhorril@outlook.com>
2019-08-08 13:19:47 +0400

    ADD Entities

---
+++ HogwartsPotions/HogwartsPotions/Data/Entities/Potion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data.Entities
+{
+    public class Potion : TimeBase
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int Rating { get; set; }
+        public DateTimeOffset IntroducedAt { get; set; }
+
+        [StringLength(100)]
+        public string PotionPhotoFileName { get; set; }
+
+    }
+}
---
+++ HogwartsPotions/HogwartsPotions/Data/Entities/TimeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data.Entities
+{
+    public class TimeBase
+    {
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
