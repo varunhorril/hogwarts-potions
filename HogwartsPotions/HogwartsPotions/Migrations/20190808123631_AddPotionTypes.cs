10d00c585e103efa310cf5b5a2a48d3bbd9eea34
Varun Horril <varunhorril@outlook.com>
2019-08-08 16:46:01 +0400

    ADD Migration: Updated field for PotionType

---
+++ HogwartsPotions/HogwartsPotions/Migrations/20190808123631_AddPotionTypes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HogwartsPotions.Migrations
+{
+    public partial class AddPotionTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Potions",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Potions",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
