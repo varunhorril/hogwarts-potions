825ab7345a793839a30d83fa820eb73921c7152e
Varun Horril <varunhorril@outlook.com>
2019-08-08 17:13:21 +0400

    ADD Model: PotionReview

---
+++ HogwartsPotions/HogwartsPotions/Data/Entities/PotionReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data.Entities
+{
+    public class PotionReview
+    {
+        public int Id { get; set; }
+        public int PotionId { get; set; }
+        public Potion Potion { get; set; }
+
+        [StringLength(100), Required]
+        public string Title { get; set; }
+        public string Review { get; set; }
+    }
+}
