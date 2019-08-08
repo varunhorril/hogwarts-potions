92a50e760b8fc45b4627c2e508b20f02ee2e3c21
Varun Horril <varunhorril@outlook.com>
2019-08-08 13:20:05 +0400

    ADD DbContext class

---
+++ HogwartsPotions/HogwartsPotions/Data/DataSeeder.cs
@@ -0,0 +1,25 @@
+﻿using HogwartsPotions.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data
+{
+    public static class DataSeeder
+    {
+        public static void Seed(this PotionDbContext dbContext)
+        {
+            if (dbContext.Potions.Any())
+            {
+
+                dbContext.Potions.Add(new Potion
+                {
+
+
+                });
+
+            }
+        }
+    }
+}
---
+++ HogwartsPotions/HogwartsPotions/Data/PotionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HogwartsPotions.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data
+{
+    public class PotionDbContext : DbContext
+    {
+        public PotionDbContext(DbContextOptions<PotionDbContext> options)
+        {
+
+        }
+
+        public DbSet<Potion> Potions { get; set; }
+    }
+}
