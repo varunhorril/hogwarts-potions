d0543ae5c692acf4eaec360e5eb2ad0e9e4b70f3
Varun Horril <varunhorril@outlook.com>
2019-08-08 16:46:42 +0400

    ADD GraphQL model: PotionTypeEnumType

--- HogwartsPotions/HogwartsPotions/GraphQL/Types/PotionType.cs
+++ HogwartsPotions/HogwartsPotions/GraphQL/Types/PotionType.cs
@@ -11,16 +11,20 @@ namespace HogwartsPotions.GraphQL.Types
     {
         public PotionType()
         {
+            #region Potion
             Field(t => t.Id);
             Field(t => t.Name).Description("The name of the potion");
             Field(t => t.Description).Description("The description of the potion");
             Field(t => t.Price).Description("Price of potion");
             Field(t => t.Stock).Description("Available number of potions in stock");
-            Field(t => t.IntroducedAt).Description("Who invented the potion? ;)");
+            Field(t => t.IntroducedAt).Description("When was the potion invented? ;)");
             Field(t => t.Rating).Description("Wizards rating of the potion");
-            Field(t => t.Type).Description("The type of potion");
+            Field<PotionTypeEnumType>("Type", "The type of potion");
+            #endregion
 
-            Field(t => t.CreatedOn);
+            #region Potion Types
+
+            #endregion
 
         }
     }
---
+++ HogwartsPotions/HogwartsPotions/GraphQL/Types/PotionTypeEnumType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.GraphQL.Types
+{
+    public class PotionTypeEnumType : EnumerationGraphType<Data.PotionType>
+    {
+        public PotionTypeEnumType()
+        {
+            Name = "Type";
+            Description = "The type of potion";
+        }
+    }
+}
