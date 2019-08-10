3c919ace68863d6cd8e0d72e1e7c592ef0179e25
Varun Horril <varunhorril@outlook.com>
2019-08-10 11:25:50 +0400

    ADD CustomException: GraphQLException

---
+++ HogwartsPotions/HogwartsPotions.Web/Exception/GraphQLException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HogwartsPotions.Web.Exception
+{
+    public class GraphQLException : ApplicationException
+    {
+        public GraphQLException(string message) : base(message)
+        {
+
+        }
+    }
+}
