45a89dab732d1c762c0ed127ce5dd95e14284b51
Varun Horril <varunhorril@outlook.com>
2019-08-10 10:50:56 +0400

    INIT Web client project.

---
+++ HogwartsPotions/HogwartsPotions.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HogwartsPotions.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
